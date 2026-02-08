8ae2bb8bf7801579edd4d89f40082000438a2e68
Chaman Prakash Kanth <007aman.p.kanth26266@gmail.com>
2026-02-08 11:35:01 +0530

    Add feed handler to fetch and return XML data

---
+++ api/feed.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  const url = req.query.url;
+
+  if (!url) {
+    return res.status(400).send("Missing url");
+  }
+
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader("Cache-Control", "s-maxage=300");
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).send("Failed to fetch feed");
+  }
+}
